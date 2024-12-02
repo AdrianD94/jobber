15152176a538d837d8d597e68b23b2097e0ad9c3
adriand94 <ady.drozman2007@gmail.com>
2024-12-02 18:03:57 +0200

    feat: fixed lint

--- apps/jobs/src/app/jobs/abstract.job.ts
+++ apps/jobs/src/app/jobs/abstract.job.ts
@@ -4,7 +4,7 @@ import { plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 import { BadRequestException } from '@nestjs/common';
 
-export abstract class AbstractJob<T extends Object> {
+export abstract class AbstractJob<T extends object> {
   private producer: Producer;
   protected abstract messageClass: new () => T;
 
@@ -15,27 +15,25 @@ export abstract class AbstractJob<T extends Object> {
     if (!this.producer) {
       this.producer = await this.pulsarClient.createProducer(job);
     }
-    if(Array.isArray(data)) {
-      for(const message of data) {
+    if (Array.isArray(data)) {
+      for (const message of data) {
         await this.send(message);
       }
       return;
     }
     return this.send(data);
-    
   }
 
-  private async send(data:T){
-    await this.producer.send({data: serialize(data)});
+  private async send(data: T) {
+    await this.producer.send({ data: serialize(data) });
   }
 
   private async validateData(data: T) {
-    
-    const errors = await validate(
-      plainToInstance(this.messageClass, data)
-    )
-    if(errors.length > 0) {
-      throw new BadRequestException(`Job data invalid: ${JSON.stringify(errors)}`);
+    const errors = await validate(plainToInstance(this.messageClass, data));
+    if (errors.length > 0) {
+      throw new BadRequestException(
+        `Job data invalid: ${JSON.stringify(errors)}`
+      );
+    }
   }
-}
 }
